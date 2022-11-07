2e36b58c237ce1fee7f1d40df874c0c30e11af3f
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2022-11-07 20:54:41 +0500

    Add files via upload

--- converters/Voroneg_UART.js
+++ converters/Voroneg_UART.js
@@ -36,13 +36,6 @@ const postfixWithEndpointName = (name, msg, definition) => {
 };
 
 const fz = {
-    set_date: {
-        cluster: 'genPowerCfg',
-        type: ['attributeReport', 'readResponse'],
-        convert: (model, msg, publish, options, meta) => {
-          return {set_date: msg.data.batteryManufacturer};
-        },
-    },
     st_text: {
         cluster: 'genMultistateValue',
         type: ['attributeReport', 'readResponse'],
@@ -57,50 +50,13 @@ const fz = {
             return result;
         },
     },
-    battery_config: {
-        cluster: 'genPowerCfg',
-        type: ['attributeReport', 'readResponse'],
-        convert: (model, msg, publish, options, meta) => {
-            const result = {};
-            if (msg.data.hasOwnProperty(0xF003)) {
-                result.battery_period = msg.data[0xF003];
-            }
-            return result;
-        },
-    },
 };
 
 const tz = {
-    set_date: {
-        // set delay after motion detector changes from occupied to unoccupied
-        key: ['set_date'],
-        convertSet: async (entity, key, value, meta) => {
-            const firstEndpoint = meta.device.getEndpoint(1);
-            await firstEndpoint.write('genPowerCfg', {batteryManufacturer: value});
-            return {state: {set_date: value}};
-        },
-        convertGet: async (entity, key, meta) => {
-            const firstEndpoint = meta.device.getEndpoint(1);
-            await firstEndpoint.read('genPowerCfg', ['batteryManufacturer']);
-        },
-    },
-    action: {
-        key: ['action'],
-        convertSet: async (entity, key, value, meta) => {
-            const firstEndpoint = meta.device.getEndpoint(1);
-            await firstEndpoint.write('genMultistateValue', {presentValue: value});
-            return {state: {action: value}};
-        },
-        convertGet: async (entity, key, meta) => {
-            const firstEndpoint = meta.device.getEndpoint(1);
-            await firstEndpoint.read('genMultistateValue', ['presentValue']);
-        },
-    },
     st_text: {
         key: ['stateText'],
         convertSet: async (entity, key, value, meta) => {
             const firstEndpoint = meta.device.getEndpoint(1);
-//            await firstEndpoint.write('genMultistateValue', {description: value});
             const payload = {14: {value, type: 0x42}};
             await firstEndpoint.write('genMultistateValue', payload);
             return {state: {stateText: value}};
@@ -108,26 +64,6 @@ const tz = {
         convertGet: async (entity, key, meta) => {
             const firstEndpoint = meta.device.getEndpoint(1);
             await firstEndpoint.read('genMultistateValue', ['stateText']);
-        },
-    },
-    change_period: {
-        key: ['battery_period'],
-        convertSet: async (entity, key, value, meta) => {
-            value *= 1;
-            const temp_value = value;
-            const payloads = {
-                battery_period: ['genPowerCfg', {0xF003: {value, type: 0x21}}],
-            };
-            await entity.write(payloads[key][0], payloads[key][1]);
-            return {
-                state: {[key]: temp_value},
-            };
-        },
-        convertGet: async (entity, key, meta) => {
-            const payloads = {
-                battery_period: ['genPowerCfg', 0xF003],
-            };
-            await entity.read(payloads[key][0], [payloads[key][1]]);
         },
     },
 };
@@ -139,18 +75,11 @@ const device = {
         description: '[UART](https://github.com/koptserg/voroneg)',
         supports: 'battery',
         fromZigbee: [
-            fz.battery_config,
-//            fz.set_date,
             fz.st_text,
-//            fromZigbeeConverters.ptvo_switch_uart,
             fromZigbeeConverters.battery,
         ],
         toZigbee: [
-            tz.change_period,
-//            tz.set_date,
-            tz.action,
             tz.st_text,
-//            toZigbeeConverters.ptvo_switch_uart,
             toZigbeeConverters.factory_reset,
         ],
         meta: {
@@ -163,38 +92,10 @@ const device = {
                 'genPowerCfg',
                 'genMultistateValue',
             ]);
-
-        const genPowerCfgPayload = [{
-                attribute: 'batteryVoltage',
-                minimumReportInterval: 0,
-                maximumReportInterval: 3600,
-                reportableChange: 0,
-            },
-            {
-                attribute: 'batteryPercentageRemaining',
-                minimumReportInterval: 0,
-                maximumReportInterval: 3600,
-                reportableChange: 0,
-            }
-        ];
-        const msBindPayload = [{
-            attribute: 'presentValue',
-            minimumReportInterval: 0,
-            maximumReportInterval: 3600,
-            reportableChange: 0,
-        }
-        ];
-
-//            await firstEndpoint.configureReporting('genPowerCfg', genPowerCfgPayload);
-//            await firstEndpoint.configureReporting('genMultistateValue', msBindPayload);
-
         },
         exposes: [
             exposes.numeric('battery', ACCESS_STATE).withUnit('%').withDescription('Remaining battery in %').withValueMin(0).withValueMax(100),
-//            exposes.text('set_date', ACCESS_STATE | ACCESS_WRITE | ACCESS_READ).withDescription('Set date (format )'),
-            exposes.text('action', ACCESS_STATE | ACCESS_WRITE | ACCESS_READ).withDescription('button clicks or data from/to UART'),
             exposes.text('stateText', ACCESS_STATE | ACCESS_WRITE | ACCESS_READ).withDescription('button clicks or data from/to UART'),
-            exposes.numeric('battery_period', ACCESS_STATE | ACCESS_WRITE | ACCESS_READ).withUnit('min').withDescription('Battery report period (default = 30 min)'),
         ],
 };
 
